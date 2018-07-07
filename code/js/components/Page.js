77551e0fc0f8a5cdcd699e78e88458cce7df914e
Yehor <nikulicov@gmail.com>
2018-07-07 22:02:02 +0300

    Add main component

---
+++ code/js/components/Page.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Header} from 'semantic-ui-react';
+
+const Page = () => (
+    <Header as='h3'>
+  Learn More
+</Header>
+);
+
+export default Page;
--- code/js/index.js
+++ code/js/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import Reducers from './reducers';
+import Page from './components/Page';
 
-const store = createStore();
+const store = createStore(Reducers);
 
 ReactDOM.render(
-  <h2>Test</h2>, 
+  //<h2>Test</h2>, 
+  <Page />,
   document.getElementById('DisplayArea')
 );

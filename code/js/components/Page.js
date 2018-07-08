f10c0f769a8ed2ea14cc567815fe51ad5e9aa123
Yehor <nikulicov@gmail.com>
2018-07-08 19:05:51 +0300

    Clear text

--- code/js/components/Page.js
+++ code/js/components/Page.js
@@ -1,16 +1,4 @@
-/*import React from 'react';
-import {Header} from 'semantic-ui-react';
-
-const Page = () => (
-    <Header as='h3'>
-  Learn More
-</Header>
-);
-
-export default Page;*/
-
 import React from 'react';
-import { Grid, Menu, Segment, Input } from 'semantic-ui-react';
 import Panel from './Panel';
 import Details from './Details';
 
@@ -22,43 +10,3 @@ const Page = () => (
       </Grid>
 );
 export default Page;
-
-/*import React, { Component } from 'react'
-import { Grid, Menu, Segment } from 'semantic-ui-react'
-
-export default class MenuExampleTabularOnLeft extends Component {
-  state = { activeItem: 'bio' }
-
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
-
-  render() {
-    const { activeItem } = this.state
-
-    return (
-      <Grid>
-        <Grid.Column width={4}>
-          <Menu fluid vertical tabular>
-            <Menu.Item name='bio' active={activeItem === 'bio'} onClick={this.handleItemClick} />
-            <Menu.Item name='pics' active={activeItem === 'pics'} onClick={this.handleItemClick} />
-            <Menu.Item
-              name='companies'
-              active={activeItem === 'companies'}
-              onClick={this.handleItemClick}
-            />
-            <Menu.Item
-              name='links'
-              active={activeItem === 'links'}
-              onClick={this.handleItemClick}
-            />
-          </Menu>
-        </Grid.Column>
-
-        <Grid.Column stretched width={12}>
-          <Segment>
-            This is an stretched grid column. This segment will always match the tab height
-          </Segment>
-        </Grid.Column>
-      </Grid>
-    )
-  }
-}*/

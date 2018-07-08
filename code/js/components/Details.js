77dafe0795aece2e9882dd615f9d6af7f812f6e8
Yehor <nikulicov@gmail.com>
2018-07-08 19:07:29 +0300

    Add conteiner Details

--- code/js/components/Details.js
+++ code/js/components/Details.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Grid, Segment } from 'semantic-ui-react';
+import DetailsConteiner from '../containers/Details';
 
 const Details = () => (
         <Grid.Column stretched width={10}>
           <Segment>
-            This is an stretched grid column. This segment will always match the tab height
+            <DetailsConteiner />
           </Segment>
         </Grid.Column>
 );

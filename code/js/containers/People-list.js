60c89b1dcda1a1dcd16631b9875bb70674cc9274
Yehor <nikulicov@gmail.com>
2018-07-08 16:04:17 +0300

    Display info from clients.json

--- code/js/containers/People-list.js
+++ code/js/containers/People-list.js
@@ -1,24 +1,36 @@
 import React, {Component} from 'react';
 import {bindActiveCreators} from 'redux';
 import {connect} from 'react-redux';
-import {Menu,Input} from 'semantic-ui-react';
+import {Menu,Input,Image, List} from 'semantic-ui-react';
 
 class PeopleList extends Component {
+    show () {
+        return this.props.peoples.map ((people, index) => {
+            return(
+                <List.Item key={index}>
+                  <Image avatar src={people.general.avatar} />
+                  <List.Content>
+                    <List.Header>{people.general.firstName}</List.Header>
+                {people.job.company}
+                  </List.Content>
+                </List.Item>
+            );
+        });
+    }
     render () {
         return (
-            <Menu fluid vertical tabular>
-                <Input icon='users' iconPosition='left' placeholder='Search people...' />
-                <Menu.Item name='bio' />
-                <Menu.Item name='pics' />
-                <Menu.Item
-                  name='companies'
-                />
-                <Menu.Item
-                  name='links'
-                />
-            </Menu>
+            <List celled size='big'>
+                <Input icon='users' fluid iconPosition='left' placeholder='Search people...' />
+                {this.show()}
+            </List>
         );
     }
 }
 
-export default PeopleList;
+function mapStateToProps(state) {
+    return {
+      peoples: state.people  
+    };
+}
+
+export default connect(mapStateToProps)(PeopleList);

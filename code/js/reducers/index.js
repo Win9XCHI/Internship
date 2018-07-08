4a451cefd26bfd4297fda0ba24980a0ca1d21943
Yehor <nikulicov@gmail.com>
2018-07-08 18:54:27 +0300

    Add active in reducers

--- code/js/reducers/index.js
+++ code/js/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import masPeople from './People';
+import ActivePeople from '../actions/people-active';
 
 const Reducers = combineReducers( {
-    people: masPeople
+    people: masPeople,
+    active: ActivePeople
 });
 
 export default Reducers;
